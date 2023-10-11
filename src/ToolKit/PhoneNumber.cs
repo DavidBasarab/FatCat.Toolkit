0bf1978187fd072c81d25be68d2ef5336d116eb3
David Basarab <dbasarab617@gmail.com>
2023-10-10 22:19:07 -0400

    wip

---
+++ src/ToolKit/PhoneNumber.cs
@@ -0,0 +1,15 @@
+﻿namespace FatCat.Toolkit;
+
+public interface IPhoneNumber
+{
+	bool Validate(string phoneNumber);
+	
+	
+}
+
+public class PhoneNumber : IPhoneNumber
+{
+	//Regex.Replace("1112224444", @"(\d{3})(\d{3})(\d{4})", "$1-$2-$3");
+	
+	public bool Validate(string phoneNumber) => throw new NotImplementedException();
+}

abaadb4dec0536369ce9416eca6b5e8ad5993fdd
David Basarab <dbasarab617@gmail.com>
2025-03-28 09:16:19 -0400

    changing the console color is what is not liked

--- src/OneOffBlazor/Program.cs
+++ src/OneOffBlazor/Program.cs
@@ -1,15 +1,17 @@
 using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using OneOffBlazor;
 
-public class Program
+namespace OneOffBlazor;
+
+public static class Program
 {
 	public static async Task Main(string[] args)
 	{
 		var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-		// builder.ConfigureContainer(new AutofacServiceProviderFactory(ConfigureContainer));
+		builder.ConfigureContainer(new AutofacServiceProviderFactory(ConfigureContainer));
 
 		builder.RootComponents.Add<App>("#app");
 		builder.RootComponents.Add<HeadOutlet>("head::after");
@@ -19,5 +21,10 @@ public class Program
 		);
 
 		await builder.Build().RunAsync();
+	}
+
+	private static void ConfigureContainer(ContainerBuilder builder)
+	{
+		Console.WriteLine("Configuring Autofac container...");
 	}
 }

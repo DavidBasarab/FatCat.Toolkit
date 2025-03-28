1e3413b8f4bdbfe0ca41338d7a87052a24b189db
David Basarab <dbasarab617@gmail.com>
2025-03-28 09:36:06 -0400

    autofac working as I want

--- src/OneOffBlazor/Program.cs
+++ src/OneOffBlazor/Program.cs
@@ -23,6 +23,8 @@ public static class Program
 		builder.Services.AddScoped(
 									sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) }
 								);
+		
+		
 
 		await builder.Build().RunAsync();
 	}
---
+++ src/OneOffBlazor/TestingService.cs
@@ -0,0 +1,28 @@
+﻿using FatCat.Toolkit.Console;
+
+namespace OneOffBlazor;
+
+public interface ITestingService
+{
+	void PrintAMessage();
+}
+
+public class TestingService : ITestingService
+{
+	public void PrintAMessage()
+	{
+		ConsoleLog.WriteMagenta("This will be a message for a console app.");
+	}
+}
+
+public class OtherTestingService : ITestingService
+{
+	private int counter;
+
+	public void PrintAMessage()
+	{
+		counter++;
+
+		ConsoleLog.WriteMagenta($"This will be a message for a console app. Counter: {counter}");
+	}
+}

fe1864387f1e4c31a772f64bd6872d2020cb02d6
David Basarab <dbasarab617@gmail.com>
2025-04-25 18:22:59 -0400

    added messenger interface

---
+++ src/ToolKit/Messaging/IMessenger.cs
@@ -0,0 +1,34 @@
+﻿namespace FatCat.Toolkit.Messaging;
+
+public interface IMessenger
+{
+	void Send<TMessage>(TMessage payload)
+		where TMessage : Message;
+
+	void Subscribe<TMessage>(Func<TMessage, Task> callback)
+		where TMessage : Message;
+
+	void Unsubscribe<TMessage>(Func<TMessage, Task> callback)
+		where TMessage : Message;
+}
+
+public class MessengerImplementation : IMessenger
+{
+	public void Send<TMessage>(TMessage payload)
+		where TMessage : Message
+	{
+		Messenger.Send(payload);
+	}
+
+	public void Subscribe<TMessage>(Func<TMessage, Task> callback)
+		where TMessage : Message
+	{
+		Messenger.Subscribe(callback);
+	}
+
+	public void Unsubscribe<TMessage>(Func<TMessage, Task> callback)
+		where TMessage : Message
+	{
+		Messenger.Unsubscribe(callback);
+	}
+}

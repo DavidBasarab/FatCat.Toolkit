955e7a61bbaed50fac3ec7148aaae9b568288a11
David Basarab <dbasarab617@gmail.com>
2025-06-04 22:20:20 -0400

    added to ConvertToUtc to DateTimeUtilties

--- src/ToolKit/DateTimeUtilities.cs
+++ src/ToolKit/DateTimeUtilities.cs
@@ -1,14 +1,24 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace FatCat.Toolkit;
 
 public interface IDateTimeUtilities
 {
+	DateTime ConvertToUtc(DateTime localDateTime);
+
 	DateTime LocalNow();
 
 	DateTime UtcNow();
 }
 
+[ExcludeFromCodeCoverage(Justification = "A time wrapper")]
 public class DateTimeUtilities : IDateTimeUtilities
 {
+	public DateTime ConvertToUtc(DateTime localDateTime)
+	{
+		return TimeZoneInfo.ConvertTimeToUtc(localDateTime);
+	}
+
 	public DateTime LocalNow()
 	{
 		return DateTime.Now;
